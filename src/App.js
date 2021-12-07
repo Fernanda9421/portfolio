c9e5418fb8f151108b1c1b6e581e033aecc3e376
Fernanda9421 <fer.candrade21@gmail.com>
2021-12-07 08:57:36 -0300

    Add Navbar

--- src/App.js
+++ src/App.js
@@ -1,10 +1,13 @@
-import logo from './logo.svg';
 import './App.css';
+import Navbar from './components/Navbar';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import 'bootstrap-icons/font/bootstrap-icons.css';
+import 'bootstrap/dist/js/bootstrap.bundle.min';
 
 function App() {
   return (
     <div className="App">
-      Início
+      <Navbar />
     </div>
   );
 }
