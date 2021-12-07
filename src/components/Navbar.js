ca357c3ef5b06fa66f4e5f1e4e7a2f8084e7fb53
Fernanda9421 <fer.candrade21@gmail.com>
2021-12-07 18:47:10 -0300

    Adiciona NavLinks e retira botao

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,40 +1,40 @@
 import React, { Component } from 'react';
+import { NavLink } from 'react-router-dom';
 
 class Navbar extends Component {
   render() {
     return (
       <nav className="navbar navbar-expand-lg navbar-dark bg-dark">
         <div className="container px-5">
-          {/* <a className="navbar-brand" href="index.html">
-            <img src={logo} alt="" width="170" />
-          </a> */}
-          <button
-            className="navbar-toggler"
-            type="button"
-            data-bs-toggle="collapse"
-            data-bs-target="#navbarSupportedContent"
-            aria-controls="navbarSupportedContent"
-            aria-expanded="false"
-            aria-label="Toggle navigation"
-          >
-            <span className="navbar-toggler-icon"></span>
-          </button>
           <div className="collapse navbar-collapse" id="navbarSupportedContent">
             <ul className="navbar-nav ms-auto mb-2 mb-lg-0">
               <li className="nav-item">
-                <a className="nav-link" href="index.html">
+                <NavLink
+                  className="nav-link"
+                  exact
+                  to="/"
+                  activeClassName="selected"
+                >
                   Home
-                </a>
+                </NavLink>
               </li>
               <li className="nav-item">
-                <a className="nav-link" href="about.html">
+                <NavLink
+                  className="nav-link"
+                  to="/about"
+                  activeClassName="selected"
+                >
                   About
-                </a>
+                </NavLink>
               </li>
               <li className="nav-item">
-                <a className="nav-link " href="/">
+                <NavLink
+                  className="nav-link"
+                  to="/portfolio"
+                  activeClassName="selected"
+                >
                   Portfolio
-                </a>
+                </NavLink>
               </li>
             </ul>
           </div>
