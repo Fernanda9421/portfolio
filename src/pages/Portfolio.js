9e21146b73524238299a86d3c053d1b1c084434b
Fernanda9421 <fer.candrade21@gmail.com>
2021-12-07 18:52:51 -0300

    Remove Navbar e footer de Portfolio.js

--- src/pages/Portfolio.js
+++ src/pages/Portfolio.js
@@ -4,70 +4,6 @@ function Portfolio() {
   return (
     <>
       <main className="flex-shrink-0">
-        {/* // Navigation */}
-        <nav className="navbar navbar-expand-lg navbar-dark bg-dark">
-          <div className="container px-5">
-            <button
-              className="navbar-toggler"
-              type="button"
-              data-bs-toggle="collapse"
-              data-bs-target="#navbarSupportedContent"
-              aria-controls="navbarSupportedContent"
-              aria-expanded="false"
-              aria-label="Toggle navigation"
-            >
-              <span className="navbar-toggler-icon"></span>
-            </button>
-            <div
-              className="collapse navbar-collapse"
-              id="navbarSupportedContent"
-            >
-              <ul className="navbar-nav ms-auto mb-2 mb-lg-0">
-                <li className="nav-item">
-                  <a className="nav-link" href="index.html">
-                    Home
-                  </a>
-                </li>
-                <li className="nav-item">
-                  <a className="nav-link" href="about.html">
-                    About
-                  </a>
-                </li>
-                <li className="nav-item dropdown">
-                  <a
-                    className="nav-link dropdown-toggle"
-                    id="navbarDropdownPortfolio"
-                    href="/"
-                    role="button"
-                    data-bs-toggle="dropdown"
-                    aria-expanded="false"
-                  >
-                    Portfolio
-                  </a>
-                  <ul
-                    className="dropdown-menu dropdown-menu-end"
-                    aria-labelledby="navbarDropdownPortfolio"
-                  >
-                    <li>
-                      <a
-                        className="dropdown-item"
-                        href="portfolio-overview.html"
-                      >
-                        Portfolio Overview
-                      </a>
-                    </li>
-                    <li>
-                      <a className="dropdown-item" href="portfolio-item.html">
-                        Portfolio Item
-                      </a>
-                    </li>
-                  </ul>
-                </li>
-              </ul>
-            </div>
-          </div>
-        </nav>
-
         {/* Conteúdo Principal */}
         <section className="py-5">
           <div className="container px-5 my-5">
@@ -150,31 +86,6 @@ function Portfolio() {
           </div>
         </section>
       </main>
-      {/* Footer */}
-      <footer className="bg-dark py-4 mt-auto">
-        <div className="container px-5">
-          <div className="row align-items-center justify-content-between flex-column flex-sm-row">
-            <div className="col-auto">
-              <div className="small m-0 text-white">
-                Copyright &copy; Your Website 2021
-              </div>
-            </div>
-            <div className="col-auto">
-              <a className="link-light small" href="#!">
-                Privacy
-              </a>
-              <span className="text-white mx-1">&middot;</span>
-              <a className="link-light small" href="#!">
-                Terms
-              </a>
-              <span className="text-white mx-1">&middot;</span>
-              <a className="link-light small" href="#!">
-                Contact
-              </a>
-            </div>
-          </div>
-        </div>
-      </footer>
     </>
   );
 }
