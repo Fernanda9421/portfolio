0f49791f5baa8558cd8f267d36e7544cb540e572
Fernanda9421 <fer.candrade21@gmail.com>
2021-12-07 18:43:08 -0300

    Adiciona rotas

--- src/App.js
+++ src/App.js
@@ -3,15 +3,19 @@ import React from 'react';
 import Footer from './components/Footer';
 import Navbar from './components/Navbar';
 import About from './pages/About';
-// import Home from './pages/Home';
-// import Portfolio from './pages/Portfolio';
-// import Form from './pages/Form';
+import { Route, Switch } from 'react-router-dom';
+import Home from './pages/Home';
+import Portfolio from './pages/Portfolio';
 
 function App() {
   return (
     <>
       <Navbar />
-      <About />
+        <Switch>
+          <Route exact path='/' component={ Home } />
+          <Route path='/about' component={ About } />
+          <Route path='/portfolio' component={ Portfolio } />
+        </Switch>
       <Footer />
     </>
   );
