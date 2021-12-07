5216518349c213a59c926897a70a6cfa4d234569
Fernanda9421 <fer.candrade21@gmail.com>
2021-12-07 09:00:17 -0300

    Add Footer

--- src/App.js
+++ src/App.js
@@ -1,13 +1,14 @@
 import './App.css';
-import Navbar from './components/Navbar';
+// import Navbar from './components/Navbar';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import 'bootstrap-icons/font/bootstrap-icons.css';
 import 'bootstrap/dist/js/bootstrap.bundle.min';
+import Footer from './components/Footer';
 
 function App() {
   return (
     <div className="App">
-      <Navbar />
+      <Footer />
     </div>
   );
 }
