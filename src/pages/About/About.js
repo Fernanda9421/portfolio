c814cbf60cb678b2cf5d011d593a73f2f756f757
Fernanda9421 <fer.candrade21@gmail.com>
2021-12-09 10:12:48 -0300

    fix lint

--- src/pages/About/About.js
+++ src/pages/About/About.js
@@ -1,9 +1,9 @@
-import '../../css/styles.css';
-import React from 'react';
-import AboutMe from './Components/AboutMe/AboutMe';
-import MyHistory from './Components/MyHistory/MyHistory';
-import GoalsAndDreams from './Components/GoalsAndDreams/GoalsAndDreams';
-import GetInTouch from './Components/GetInTouch/GetInTouch';
+import "../../css/styles.css";
+import React from "react";
+import AboutMe from "./Components/AboutMe/AboutMe";
+import MyHistory from "./Components/MyHistory/MyHistory";
+import GoalsAndDreams from "./Components/GoalsAndDreams/GoalsAndDreams";
+import GetInTouch from "./Components/GetInTouch/GetInTouch";
 
 class About extends React.Component {
   render() {
