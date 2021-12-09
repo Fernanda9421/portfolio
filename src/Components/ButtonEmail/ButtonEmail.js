950a9fdfaea4ee0117c8eef12a473d41f009c1be
Fernanda9421 <fer.candrade21@gmail.com>
2021-12-09 08:33:41 -0300

    Cria pasta e adiciona arquivo para botao de email

---
+++ src/Components/ButtonEmail/ButtonEmail.js
@@ -0,0 +1,34 @@
+import React, { Component } from 'react';
+import Modal from '../Modal/Modal';
+
+class ButtonEmail extends Component {
+  constructor() {
+    super();
+
+    this.state = {
+      isOpen: false,
+    };
+  }
+
+  render() {
+    const { isOpen } = this.state;
+    return (
+      <>
+      <button
+          type="button"
+          className="btn btn-ligth"
+          onClick={() => this.setState({ isOpen: true })}
+        >
+          <h2><i class="bi bi-envelope-check"></i></h2>
+        </button>
+        <Modal isOpen={isOpen} onClickClose={() => this.setState({ isOpen: false })}>
+          <h3 className="text-center">Email para contato</h3>
+          <hr />
+          <h5 className="text-center pt-4 pb-2"><em>fer.candrade21@gmail.com</em></h5>
+        </Modal>
+        </>
+    );
+  }
+}
+
+export default ButtonEmail;
