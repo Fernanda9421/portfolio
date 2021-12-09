b3d68e51d32d515db75d2c7934640c08986bc724
Fernanda9421 <fer.candrade21@gmail.com>
2021-12-09 10:32:15 -0300

    Cria componente Header em Home.js

--- src/pages/Home/Home.js
+++ src/pages/Home/Home.js
@@ -1,47 +1,13 @@
 import React, { Component } from "react";
 import fernanda from "../../assets/images/fernanda.jpeg";
+import Header from "./Components/Header";
 
 class Home extends Component {
   render() {
     return (
       <>
         <main className="flex-shrink-0">
-          <header className="bg-dark py-5">
-            <div className="container px-5">
-              <div className="row gx-5 align-items-center justify-content-center">
-                <div className="col-lg-8 col-xl-7 col-xxl-6">
-                  <div className="my-5 text-center text-xl-start">
-                    <h1 className="display-5 fw-bolder text-white mb-2">
-                      Tryberfólio | Meus Projetos
-                    </h1>
-                    <p className="lead fw-normal text-white-50 mb-4">
-                      Quickly design and customize responsive mobile-first sites with
-                      Bootstrap, the world`s most popular front-end open source
-                      toolkit!
-                    </p>
-                    <div className="d-grid gap-3 d-sm-flex justify-content-sm-center justify-content-xl-start">
-                      <a
-                        className="btn btn-success btn-lg px-4 me-sm-3"
-                        href="#features"
-                      >
-                        Repositórios
-                      </a>
-                      <a className="btn btn-outline-light btn-lg px-4" href="#!">
-                        Sobre
-                      </a>
-                    </div>
-                  </div>
-                </div>
-                <div className="col-xl-5 col-xxl-6 d-none d-xl-block text-center">
-                  <img
-                    className="img-fluid rounded-3 my-5"
-                    src="https://dummyimage.com/600x400/343a40/6c757d"
-                    alt="..."
-                  />
-                </div>
-              </div>
-            </div>
-          </header>
+          <Header />
           {/* // Features section */}
           <section className="py-5" id="features">
             <div className="container px-5 my-5">
