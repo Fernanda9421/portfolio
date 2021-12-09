94d0dae578d024d96cdf40b8803ce83582f58463
Fernanda9421 <fer.candrade21@gmail.com>
2021-12-09 10:34:59 -0300

    Cria Header para pagina Home

---
+++ src/pages/Home/Components/Header.js
@@ -0,0 +1,26 @@
+import React, { Component } from "react";
+import ButtonsHeader from "./ButtonsHeader";
+import HeaderDescription from "./HeaderDescription";
+import ImageHeader from "./ImageHeader";
+
+class Header extends Component {
+  render() {
+    return (
+      <header className="bg-dark py-5">
+        <div className="container px-5">
+          <div className="row gx-5 align-items-center justify-content-center">
+            <div className="col-lg-8 col-xl-7 col-xxl-6">
+              <div className="my-5 text-center text-xl-start">
+                <HeaderDescription />
+                <ButtonsHeader />
+              </div>
+            </div>
+            <ImageHeader />
+          </div>
+        </div>
+      </header>
+    );
+  }
+}
+
+export default Header;
