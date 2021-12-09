bf5af110bbad765192e1546c177fde5b9c99ecfa
Fernanda9421 <fer.candrade21@gmail.com>
2021-12-09 13:54:04 -0300

    Cria arquivo de soft skills

---
+++ src/Data/softSkills.js
@@ -0,0 +1,24 @@
+const softSkills = [
+  {
+    skill: "Autoconhecimento",
+    image: "https://blog.portalpos.com.br/app/uploads/2021/06/autoconhecimento.jpg",
+  },
+  {
+    skill: "Empatia",
+    image: "https://www.psicoglobal.com/img/uploads/2019/empatia-cabezas.jpg",
+  },
+  {
+    skill: "Resiliência",
+    image: "https://static.significados.com.br/foto/flor-resiliente-no-asfalto-cke.jpg",
+  },
+  {
+    skill: "Autogestão",
+    image: "http://www.abrhrs.org.br/blog/wp-content/uploads/2018/09/gestao-equipes-autogestao.jpeg",
+  },
+  {
+    skill: "Escuta ativa",
+    image: "https://www.queroevoluir.com.br/wp-content/uploads/2020/09/escuta-ativa.jpg",
+  },
+];
+
+export default softSkills;
