4050c0109cae7440e30756bda5f9de18440c97f4
Fernanda9421 <fer.candrade21@gmail.com>
2021-12-09 08:41:56 -0300

    Cria diretorio para pagina About

---
+++ src/pages/About/About.js
@@ -0,0 +1,23 @@
+import '../../css/styles.css';
+import React from 'react';
+import AboutMe from './Components/AboutMe/AboutMe';
+import MyHistory from './Components/MyHistory/MyHistory';
+import GoalsAndDreams from './Components/GoalsAndDreams/GoalsAndDreams';
+import GetInTouch from './Components/GetInTouch/GetInTouch';
+
+class About extends React.Component {
+  render() {
+    return (
+      <>
+        <main className="flex-shrink-0">
+          <AboutMe />
+          <MyHistory />
+          <GoalsAndDreams />
+          <GetInTouch />
+        </main>
+      </>
+    );
+  }
+}
+
+export default About;
