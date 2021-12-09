f338a90db09e1c8c77fb917138c05c48ba7277e8
Fernanda9421 <fer.candrade21@gmail.com>
2021-12-09 13:53:45 -0300

    Cria arquivo de hard skills

---
+++ src/Data/hardSkills.js
@@ -0,0 +1,40 @@
+const hardSkills = [
+  {
+    skill: "Unix & Bash",
+    image: "https://www.neoteo.com/wp-content/uploads/2014/09/capture44.png",
+  },
+  {
+    skill: "Git",
+    image: "https://www.embarcados.com.br/wp-content/uploads/2015/02/imagem-de-destaque-39-850x510.png",
+  },
+  {
+    skill: "Github",
+    image: "https://gizmodo.uol.com.br/wp-content/blogs.dir/8/files/2020/06/github.jpg",
+  },
+  {
+    skill: "HTML",
+    image: "https://arquivo.devmedia.com.br/marketing/img/guia-html-38051.png",
+  },
+  {
+    skill: "CSS",
+    image: "https://www.somosadd.com.br/wp-content/uploads/2021/05/css-outsystems.png",
+  },
+  {
+    skill: "JavaScript",
+    image: "https://lh3.googleusercontent.com/proxy/ETDR2K1hTNuIXSvZz0TVul6IopLMjJt_wN4pFa6oTxhVS3qOh0Wg8TOAClVa8rvNof27xW3r_CA7MAkYQNlLHQDgFBKFhi-GwBjw7niQwpQKllLN5k5guYEuReGY-i5l2GDyHjpJ1DstjfbZdeI",
+  },
+  {
+    skill: "Testes Unitários - Jest",
+    image: "https://miro.medium.com/max/796/1*VxBKV4bcUJ4M7WCuCNmHWw.png",
+  },
+  {
+    skill: "React",
+    image: "https://hermes.digitalinnovation.one/articles/cover/f60b2c55-e681-42cb-95bc-e9b084da3867.png",
+  },
+  {
+    skill: "React Testing Library (RTL)",
+    image: "https://miro.medium.com/max/500/1*FdcfXXlYDEDNGToFjA_B4w.jpeg",
+  },
+];
+
+export default hardSkills;
