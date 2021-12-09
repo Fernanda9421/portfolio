c1367148015c053fd05a7700cad2ce81caba31b5
Fernanda9421 <fer.candrade21@gmail.com>
2021-12-09 08:42:43 -0300

    Cria diretorio para componentes de About

---
+++ src/pages/About/Components/GetInTouch/GetInTouch.js
@@ -0,0 +1,22 @@
+import React, { Component } from 'react';
+import ButtonsContact from '../../../../Components/ButtonsContact/ButtonsContact';
+
+class GetInTouch extends Component {
+  render() {
+    return (
+      <section className="py-5 bg-light">
+        <div className="container px-5 my-5">
+          <div className="text-center">
+            <h2 className="fw-bolder">Contatos</h2>
+            <p className="lead fw-normal text-muted mb-5">
+              Onde me encontrar
+            </p>
+          </div>
+          <ButtonsContact />
+        </div>
+      </section>
+    );
+  }
+}
+
+export default GetInTouch;
