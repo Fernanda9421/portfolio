befa14771dee2efa12d238f8d706f43d874cd109
Fernanda9421 <fer.candrade21@gmail.com>
2021-12-10 15:05:24 -0300

    Adicina cor de fundo

--- src/pages/Home/Components/HardSkillsSection/HardSkillsSection.js
+++ src/pages/Home/Components/HardSkillsSection/HardSkillsSection.js
@@ -4,23 +4,25 @@ import ListHardSkills from "../../../../Components/ListHardSkills/ListHardSkills
 class HardSkillsSection extends Component {
   render() {
     return (
-      <section className="py-5" id="features">
-            <div className="container px-5 my-5">
-              <div className="row gx-5">
-                <div className="col-lg-4 mb-5 mb-lg-0 d-flex flex-column justify-content-center">
-                  <h2 className="fw-bolder mb-0">
-                    Hard Skills
-                  </h2>
-                  <span className="d-flex mt-3">Habilidades Técnicas Desenvolvidas</span>
-                </div>
-                <div className="col-lg-8 list-hard-skills">
-                  <div className="row gx-5 row-cols-1 row-cols-md-3 d-flex justify-content-center">
-                    <ListHardSkills />
-                  </div>
+      <div className="py-5 bg-light">
+        <section className="py-5" id="features">
+          <div className="container px-5 my-5">
+            <div className="row gx-5">
+              <div className="col-lg-4 mb-5 mb-lg-0 d-flex flex-column justify-content-center">
+                <h2 className="fw-bolder mb-0">
+                  Hard Skills
+                </h2>
+                <span className="d-flex mt-3">Habilidades Técnicas Desenvolvidas</span>
+              </div>
+              <div className="col-lg-8 list-hard-skills">
+                <div className="row gx-5 row-cols-1 row-cols-md-3 d-flex justify-content-center">
+                  <ListHardSkills />
                 </div>
               </div>
             </div>
-          </section>
+          </div>
+        </section>
+      </div>
     );
   }
 }
