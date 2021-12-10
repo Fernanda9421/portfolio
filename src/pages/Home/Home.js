ad1a933e517c0c40eb4a478bc63ba26b2a263278
Fernanda9421 <fer.candrade21@gmail.com>
2021-12-10 12:56:51 -0300

    Ajusta classes de bootstrap

--- src/pages/Home/Home.js
+++ src/pages/Home/Home.js
@@ -1,7 +1,6 @@
 import React, { Component } from "react";
 import fernanda from "../../assets/images/fernanda.jpeg";
 import ListHardSkills from "../../Components/ListHardSkills/ListHardSkills";
-// import ListHardSkills from "../../Components/ListHardSkills/ListHardSkills";
 import Header from "./Components/Header/Header";
 import "./home.css";
 
@@ -15,13 +14,14 @@ class Home extends Component {
           <section className="py-5" id="features">
             <div className="container px-5 my-5">
               <div className="row gx-5">
-                <div className="col-lg-4 mb-5 mb-lg-0 bg-dark text-white">
-                  <h2 className="fw-bolder mb-0 hard-skills">
+                <div className="col-lg-4 mb-5 mb-lg-0 d-flex flex-column justify-content-center">
+                  <h2 className="fw-bolder mb-0">
                     Hard Skills
                   </h2>
+                  <span className="d-flex mt-3">Habilidades Técnicas Desenvolvidas</span>
                 </div>
                 <div className="col-lg-8 list-hard-skills">
-                  <div className="row gx-5 row-cols-1 row-cols-md-4">
+                  <div className="row gx-5 row-cols-1 row-cols-md-3 d-flex justify-content-center">
                     <ListHardSkills />
                     {/* <div className="col mb-5 h-100">
                       <div className="feature bg-success bg-gradient text-white rounded-3 mb-3">
