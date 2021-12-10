74caa7798658e244b22efac8c9e603a96afb9c24
Fernanda9421 <fer.candrade21@gmail.com>
2021-12-10 13:22:34 -0300

    Componentizaçao das Hard Skills

---
+++ src/pages/Home/Components/HardSkillsSection/HardSkillsSection.js
@@ -0,0 +1,28 @@
+import React, { Component } from "react";
+import ListHardSkills from "../../../../Components/ListHardSkills/ListHardSkills";
+
+class HardSkillsSection extends Component {
+  render() {
+    return (
+      <section className="py-5" id="features">
+            <div className="container px-5 my-5">
+              <div className="row gx-5">
+                <div className="col-lg-4 mb-5 mb-lg-0 d-flex flex-column justify-content-center">
+                  <h2 className="fw-bolder mb-0">
+                    Hard Skills
+                  </h2>
+                  <span className="d-flex mt-3">Habilidades Técnicas Desenvolvidas</span>
+                </div>
+                <div className="col-lg-8 list-hard-skills">
+                  <div className="row gx-5 row-cols-1 row-cols-md-3 d-flex justify-content-center">
+                    <ListHardSkills />
+                  </div>
+                </div>
+              </div>
+            </div>
+          </section>
+    );
+  }
+}
+
+export default HardSkillsSection;
