d8c6504bf655d14822f22f7a749769f48028e7ca
Fernanda9421 <fer.candrade21@gmail.com>
2021-12-10 19:27:36 -0300

    Ajusta texto descritivo

--- src/pages/Home/Components/Header/HeaderDescription.js
+++ src/pages/Home/Components/Header/HeaderDescription.js
@@ -1,11 +1,12 @@
 import React, { Component } from "react";
+import "./headerDescription.css";
 
 class HeaderDescription extends Component {
   render() {
     return (
       <>
-        <h1 className="display-5 fw-bolder text-white mb-2">
-          Portfolio | Meus Projetos
+        <h1 className="line typing-animation">
+          Olá, eu sou Fernanda Andrade! =)
         </h1>
         <p className="lead fw-normal text-white-50 mb-4">
           Quickly design and customize responsive mobile-first sites with
