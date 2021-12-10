c130311c2befbd09e9732d90b1941ef219ad5881
Fernanda9421 <fer.candrade21@gmail.com>
2021-12-10 13:24:59 -0300

    Atualiza componente Home

--- src/pages/Home/Home.js
+++ src/pages/Home/Home.js
@@ -1,7 +1,8 @@
 import React, { Component } from "react";
 import fernanda from "../../assets/images/fernanda.jpeg";
-import ListHardSkills from "../../Components/ListHardSkills/ListHardSkills";
+import HardSkillsSection from "./Components/HardSkillsSection/HardSkillsSection";
 import Header from "./Components/Header/Header";
+import SoftSkillsSection from "./Components/SoftSkillsSection/SoftSkillsSection";
 import "./home.css";
 
 class Home extends Component {
@@ -10,64 +11,10 @@ class Home extends Component {
       <>
         <main className="flex-shrink-0">
           <Header />
-          {/* // Features section */}
-          <section className="py-5" id="features">
-            <div className="container px-5 my-5">
-              <div className="row gx-5">
-                <div className="col-lg-4 mb-5 mb-lg-0 d-flex flex-column justify-content-center">
-                  <h2 className="fw-bolder mb-0">
-                    Hard Skills
-                  </h2>
-                  <span className="d-flex mt-3">Habilidades Técnicas Desenvolvidas</span>
-                </div>
-                <div className="col-lg-8 list-hard-skills">
-                  <div className="row gx-5 row-cols-1 row-cols-md-3 d-flex justify-content-center">
-                    <ListHardSkills />
-                    {/* <div className="col mb-5 h-100">
-                      <div className="feature bg-success bg-gradient text-white rounded-3 mb-3">
-                        <i className="bi bi-grid-1x2-fill"></i>
-                      </div>
-                      <h2 className="h5">Day 1 - Components, Props, State</h2>
-                      <p className="mb-0">
-                        Paragraph of text beneath the heading to explain the heading.
-                        Here is just a bit more text.
-                      </p>
-                    </div>
-                    <div className="col mb-5 h-100">
-                      <div className="feature bg-success bg-gradient text-white rounded-3 mb-3">
-                        <i className="bi bi-router"></i>
-                      </div>
-                      <h2 className="h5">Day 2 - Routes V5</h2>
-                      <p className="mb-0">
-                        Paragraph of text beneath the heading to explain the heading.
-                        Here is just a bit more text.
-                      </p>
-                    </div>
-                    <div className="col mb-5 mb-md-0 h-100">
-                      <div className="feature bg-success bg-gradient text-white rounded-3 mb-3">
-                        <i className="bi bi-recycle"></i>
-                      </div>
-                      <h2 className="h5">Day 3 - Lifecycle + API</h2>
-                      <p className="mb-0">
-                        Paragraph of text beneath the heading to explain the heading.
-                        Here is just a bit more text.
-                      </p>
-                    </div>
-                    <div className="col h-100">
-                      <div className="feature bg-success bg-gradient text-white rounded-3 mb-3">
-                        <i className="bi bi-card-checklist"></i>
-                      </div>
-                      <h2 className="h5">Day 4 - Tests RTL</h2>
-                      <p className="mb-0">
-                        Paragraph of text beneath the heading to explain the heading.
-                        Here is just a bit more text.
-                      </p>
-                    </div> */}
-                  </div>
-                </div>
-              </div>
-            </div>
-          </section>
+
+          <HardSkillsSection />
+          <SoftSkillsSection />
+          
           {/* Testimonial section */}
           <div className="py-5 bg-light">
             <div className="container px-5 my-5">
