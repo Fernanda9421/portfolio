fd6a63139fed4c54667ee8e346bdaba7d28afd8e
Fernanda9421 <fer.candrade21@gmail.com>
2021-12-15 07:16:19 -0300

    Adiciona texto de apresentaçao pessoal

--- src/pages/About/Components/AboutMe/AboutMe.js
+++ src/pages/About/Components/AboutMe/AboutMe.js
@@ -10,7 +10,10 @@ class AboutMe extends Component {
               <div className="my-5">
                 <h1 className="fw-bolder mb-3">Sobre mim</h1>
                 <p className="lead fw-normal text-muted mb-4">
-                  Breve apresentação pessoal
+                  Olá, meu nome é Fernanda.
+                  <p>Atualmente, sou estudante de Desenvolvimento Web.</p>
+                  <p>Não me defino pelo que fiz até o momento, e sim pelo trabalho que quero fazer. Continuo aprendendo e me desafiando todos os dias, e sigo fazendo o que realmente importa para mim.</p>
+                  <p>Acredito que, quando estamos em nossa zona de conforto, não sabemos do que realmente somos capazes, e por isso, me motivo à procurar algo novo diariamente.</p>
                 </p>
               </div>
             </div>
