21b603b962b0e25718f0eca768f361f912595768
Fernanda9421 <fer.candrade21@gmail.com>
2021-12-15 14:11:41 -0300

    Altera imagem de header

--- src/pages/Home/Components/Header/ImageHeader.js
+++ src/pages/Home/Components/Header/ImageHeader.js
@@ -1,12 +1,13 @@
 import React, { Component } from "react";
+import fer from "../../../../assets/images/fer.jpeg";
 
 class ImageHeader extends Component {
   render() {
     return (
       <div className="col-xl-5 col-xxl-6 d-none d-xl-block text-center">
         <img
-          className="img-fluid rounded-3 my-5"
-          src="https://dummyimage.com/600x400/343a40/6c757d"
+          className="img-fluid rounded-circle my-5"
+          src={fer}
           alt="..."
         />
       </div>
