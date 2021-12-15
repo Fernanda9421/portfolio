b028b0e7fd70260166f198231126a7114a9287b3
Fernanda9421 <fer.candrade21@gmail.com>
2021-12-15 14:14:13 -0300

    Adiciona renderizaçao condicional nos botoes de qualificaçao

--- src/pages/Home/Components/Qualifications/QualificationsTabs.js
+++ src/pages/Home/Components/Qualifications/QualificationsTabs.js
@@ -1,23 +1,48 @@
 import React, { Component } from "react";
+import QualificationsSection from "./QualificationsSection";
 import "./qualificationsTabs.css";
+import WorkSection from "./WorkSection";
 
 class QualificationsTabs extends Component {
+  constructor() {
+    super();
+
+    this.state = {
+      isClickedEducation: true,
+      isClickedCarrer: false,
+    };
+  }
+
   render() {
+    const { isClickedEducation } = this.state;
     return (
-      <div className="d-flex justify-content-center">
-        <div>
-          <button className="btn btn-light d-flex align-items-center text-muted me-5">
-            <h3><i className="bi bi-mortarboard icons" /></h3>
-            Educação
-          </button>
+      <>
+        <div className="d-flex justify-content-center mb-5">
+          <div>
+            <button
+              onClick={() => this.setState({ isClickedEducation: true, isClickedCarrer: false })}
+              className="btn btn-light d-flex align-items-center text-muted me-5"
+            >
+              <h3><i className="bi bi-mortarboard icons" /></h3>
+              Educação
+            </button>
+          </div>
+          <div>
+            <button
+              onClick={() => this.setState({ isClickedEducation: false, isClickedCarrer: true })}
+              className="btn btn-light d-flex align-items-center text-muted button">
+              <h3><i className="bi bi-briefcase icons" /></h3>
+              Carreira
+            </button>
+          </div>
         </div>
-        <div>
-          <button className="btn btn-light d-flex align-items-center text-muted button">
-            <h3><i className="bi bi-briefcase icons" /></h3>
-            Carreira
-          </button>
-        </div>
-      </div>
+        <>
+          {
+            isClickedEducation ?
+              <QualificationsSection /> : <WorkSection />
+          }
+        </>
+      </>
     );
   }
 }
