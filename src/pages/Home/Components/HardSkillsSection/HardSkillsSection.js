e038268e9e44cbab3ad0d757b566519b14e8a7eb
Fernanda9421 <fer.candrade21@gmail.com>
2021-12-15 07:17:30 -0300

    Adiciona icon em hard skills

--- src/pages/Home/Components/HardSkillsSection/HardSkillsSection.js
+++ src/pages/Home/Components/HardSkillsSection/HardSkillsSection.js
@@ -4,12 +4,13 @@ import ListHardSkills from "../../../../Components/ListHardSkills/ListHardSkills
 class HardSkillsSection extends Component {
   render() {
     return (
-      <div className="py-5 bg-light">
+      <div className="py-4 bg-light">
         <section className="py-5" id="features">
           <div className="container px-5 my-5">
             <div className="row gx-5">
               <div className="col-lg-4 mb-5 mb-lg-0 d-flex flex-column justify-content-center">
                 <h2 className="fw-bolder mb-0">
+                  <p><i className="bi bi-code" /></p>
                   Hard Skills
                 </h2>
                 <span className="d-flex mt-3">Habilidades Técnicas Desenvolvidas</span>
