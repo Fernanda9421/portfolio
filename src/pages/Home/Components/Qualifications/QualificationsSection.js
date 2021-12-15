f8fb8c8b0240a8116be94c622749c8dec9684282
Fernanda9421 <fer.candrade21@gmail.com>
2021-12-15 14:13:37 -0300

    Altera posicionamento de divs

--- src/pages/Home/Components/Qualifications/QualificationsSection.js
+++ src/pages/Home/Components/Qualifications/QualificationsSection.js
@@ -6,22 +6,24 @@ class QualificationsSection extends Component {
     return (
       <div className="qualification__container">
         <div className="qualification__divOne lh-base">
-          <h5 className="text-muted">Nutrição</h5>
-          <span className="text-muted">Universidade Federal de Lavras</span>
-          <div className="text-muted">
+          <div className="text-muted qualification__calendar">
             <i className="bi bi-calendar3 me-3 text-muted" />
             2013 - 2018
+            <hr />
           </div>
+          <h6 className="qualification__title">Nutrição</h6>
+          <span className="text-muted">Universidade Federal de Lavras</span>
         </div>
         <div className="qualification__divTwo" />
         <div className="qualification__divThree" />
         <div className="qualification__divFour lh-base">
-        <h5 className="text-muted">Desenvolvimento Web | FullStack</h5>
-          <span className="text-muted">Trybe - Escola de Programação</span>
           <div className="text-muted">
             <i className="bi bi-calendar3 me-3 text-muted" />
             2021 - o momento
+            <hr />
           </div>
+          <h6 className="qualification__title">Desenvolvimento Web | FullStack</h6>
+          <span className="text-muted">Trybe - Escola de Programação</span>
         </div>
       </div>
     );
