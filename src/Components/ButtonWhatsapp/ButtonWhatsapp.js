35d8298886dcce11f1693d184385463819eb7b4f
Fernanda9421 <fer.candrade21@gmail.com>
2021-12-16 13:57:13 -0300

    Altera link para whatsapp

--- src/Components/ButtonWhatsapp/ButtonWhatsapp.js
+++ src/Components/ButtonWhatsapp/ButtonWhatsapp.js
@@ -1,32 +1,15 @@
 import React, { Component } from "react";
-import Modal from "../Modal/Modal";
 
 class ButtonWhatsapp extends Component {
-  constructor() {
-    super();
-
-    this.state = {
-      isOpenWpp: false,
-    };
-  }
-
   render() {
-    const { isOpenWpp } = this.state;
     return (
-      <>
-        <button
-          type="button"
-          className="btn btn-ligth"
-          onClick={() => this.setState({ isOpenWpp: true })}
-        >
-          <h2><i className="bi bi-whatsapp"></i></h2>
+      <button type="button" className="btn btn-ligth">
+          <a
+            href="http://wa.me/55035992068181"
+            rel="noreferrer" target="_blank">
+            <h2><i className="bi bi-whatsapp" /></h2>
+          </a>
         </button>
-        <Modal isOpen={isOpenWpp} onClickClose={() => this.setState({ isOpenWpp: false })}>
-          <h3 className="text-center">Número para contato</h3>
-          <hr />
-          <h5 className="text-center pt-4 pb-2"><em>(35) 99206 8181</em></h5>
-        </Modal>
-        </>
     );
   }
 }
