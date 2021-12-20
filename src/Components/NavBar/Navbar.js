d70ed8d2ef68f57ec70a3bc43cdb283ca4334c54
Fernanda9421 <fer.candrade21@gmail.com>
2021-12-20 10:36:54 -0300

    Adiciona logo e cria link para home

--- src/Components/NavBar/Navbar.js
+++ src/Components/NavBar/Navbar.js
@@ -5,9 +5,20 @@ import "./navbar.css";
 class Navbar extends Component {
   render() {
     return (
-      <nav className="navbar navbar-expand-lg navbar-dark bg-dark">
+      <nav className="navbar navbar-expand-lg navbar-dark pb-2 pt-2 navbar__header">
         <div className="container px-5">
           <div className="collapse navbar-collapse" id="navbarSupportedContent">
+            <NavLink
+              className="nav-link"
+              exact
+              to="/"
+            >
+              <div className="navbarLogo">
+                <p className="logo">Fernanda Andrade</p>
+                <p className="logo-subtitle">Portfolio Pessoal</p>
+              </div>
+            </NavLink>
+
             <ul className="navbar-nav ms-auto mb-2 mb-lg-0">
               <li className="nav-item">
                 <NavLink
