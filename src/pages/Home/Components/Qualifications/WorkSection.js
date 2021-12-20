992f1fc7ecec5d29fb6c20602f99392fc798d598
Fernanda9421 <fer.candrade21@gmail.com>
2021-12-20 10:37:17 -0300

    Atualiza qualificaçoes

--- src/pages/Home/Components/Qualifications/WorkSection.js
+++ src/pages/Home/Components/Qualifications/WorkSection.js
@@ -8,22 +8,22 @@ class WorkSection extends Component {
         <div className="qualification__divOne lh-base">
           <div className="text-muted">
             <i className="bi bi-calendar3 me-3 text-muted" />
-            2020 - 2021
+            2018 - 2020
             <hr />
           </div>
           <h6 className="qualification__title">Gerente de Unidade | Nutricionista</h6>
-          <span className="text-muted">Momo Confeitaria - Belo Horizonte, MG</span>
+          <span className="text-muted">Alibraz - Belo Horizonte, MG</span>
         </div>
         <div className="qualification__divTwo" />
         <div className="qualification__divThree" />
         <div className="qualification__divFour lh-base">
           <div className="text-muted">
             <i className="bi bi-calendar3 me-3 text-muted" />
-            2018 - 2020
+            2020 - 2021
             <hr />
           </div>
           <h6 className="qualification__title">Gerente de Unidade | Nutricionista</h6>
-          <span className="text-muted">Alibraz - Belo Horizonte, MG</span>
+          <span className="text-muted">Momo Confeitaria - Belo Horizonte, MG</span>
         </div>
       </div>
     );
