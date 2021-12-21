5b43646a548362a0337afae9d0e6f60b8f8e9b6b
Fernanda9421 <fer.candrade21@gmail.com>
2021-12-21 16:56:19 -0300

    Adiciona Data com informaçoes dos projetos disponiveis

---
+++ src/Data/projects.js
@@ -0,0 +1,13 @@
+import pixelsArt from "../assets/projects/pixels-art.png";
+
+const projects = [
+  {
+    id: 1,
+    name: "Pixels Art",
+    image: pixelsArt,
+    link: "https://pixels-art-three.vercel.app/",
+    github: "https://github.com/Fernanda9421/pixels-art",
+  },
+];
+
+export default projects;
