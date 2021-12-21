f20315e371a39964adefea5427537a19affa5d23
Fernanda9421 <fer.candrade21@gmail.com>
2021-12-21 16:57:32 -0300

    Cria componente que renderiza os projetos disponiveis

---
+++ src/pages/Portfolio/Components/CardProject/CardProject.js
@@ -0,0 +1,45 @@
+import React, { Component } from "react";
+import PropTypes from "prop-types";
+import "./cardProject.css";
+
+class CardProject extends Component {
+  render() {
+    const { project } = this.props;
+
+    return (
+      <div className="col-lg-6">
+        <div className="position-relative mb-5">
+          <img
+            className="img-fluid rounded-3 mb-3"
+            src={project.image}
+            alt={project.name}
+          />
+          <h2 className="text-muted cardProject__title">{project.name}</h2>
+          <div className="mt-4 cardProject__links">
+            <a
+              href={project.github}
+              rel="noreferrer"
+              target="_blank"
+            >
+              <h3><i className="text-muted bi bi-github" /></h3>
+            </a>
+            <a
+              className="cardProject__linkProject"
+              href={project.link}
+              rel="noreferrer"
+              target="_blank"
+            >
+              Ver Projeto <i className="bi bi-chevron-double-right" />
+            </a>
+          </div>
+        </div>
+      </div>
+    );
+  }
+}
+
+export default CardProject;
+
+CardProject.propTypes = {
+  project: PropTypes.object.isRequired,
+};
