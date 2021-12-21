32f5ab5d9932e54b902ce5468220e8d8d982fabc
Fernanda9421 <fer.candrade21@gmail.com>
2021-12-21 08:54:13 -0300

    Atualiza apresentaçao pessoal

--- src/pages/About/Components/AboutMe/AboutMe.js
+++ src/pages/About/Components/AboutMe/AboutMe.js
@@ -10,9 +10,9 @@ class AboutMe extends Component {
               <div className="my-5">
                 <h1 className="fw-bolder mb-3">Sobre mim</h1>
                 <p className="lead fw-normal text-muted mb-4">
-                  Olá, meu nome é Fernanda.
+                  Olá, meu nome é Fernanda, tenho 27 anos e moro em Belo Horizonte, Minas Gerais.
                   <p>Atualmente, sou estudante de Desenvolvimento Web.</p>
-                  <p>Não me defino pelo que fiz até o momento, e sim pelo trabalho que quero fazer. Continuo aprendendo e me desafiando todos os dias, e sigo fazendo o que realmente importa para mim.</p>
+                  <p>Não gosto de me definir pelo que fiz até o momento, e sim pelo trabalho que quero fazer. Continuo aprendendo e me desafiando todos os dias, e sigo fazendo o que realmente importa para mim.</p>
                   <p>Acredito que, quando estamos em nossa zona de conforto, não sabemos do que realmente somos capazes, e por isso, me motivo à procurar algo novo diariamente.</p>
                 </p>
               </div>
