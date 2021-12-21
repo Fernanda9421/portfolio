d99eca77d749fd4539699ad37895fd544192d987
Fernanda9421 <fer.candrade21@gmail.com>
2021-12-21 08:55:48 -0300

    Adiciona imagens e classes para estilizçao

--- src/pages/About/Components/MyHistory/MyHistory.js
+++ src/pages/About/Components/MyHistory/MyHistory.js
@@ -1,5 +1,6 @@
 import React, { Component } from "react";
 import "./myHistory.css";
+import photoHistory from "../../../../assets/images/myHistory.jpg";
 
 class MyHistory extends Component {
   render() {
@@ -9,13 +10,13 @@ class MyHistory extends Component {
           <div className="row gx-5 align-items-center">
             <div className="col-lg-6">
               <img
-                className="img-fluid rounded mb-5 mb-lg-0"
-                src="https://dummyimage.com/600x400/343a40/6c757d"
+                className="img-fluid myHistory__photo"
+                src={ photoHistory }
                 alt="..."
               />
             </div>
-            <div className="col-lg-6">
-              <h3>Minha História</h3>
+            <div className="col-lg-6 myHistory__description">
+              <h3 className="myHistory__title">Minha História</h3>
               <p className="lead fw-normal text-muted mb-0">
                 Nasci e morei grande parte da minha vida em uma pequena cidade no Sul de Minas Gerais. Desde pequena, sempre incentivada pelos meus pais, soube que educação é o bem mais precioso que podemos ter.
                 <p>Em 2008, me formei em Nutrição pela Universidade Federal de Lavras. Foram anos intensos, e de muito aprendizado.</p>
