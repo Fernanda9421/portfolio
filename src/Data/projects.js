047584c41fe936ad02566e90cc3cb81459fdd15c
Fernanda9421 <fer.candrade21@gmail.com>
2021-12-22 17:55:33 -0300

    Adiciona projeto Trybe Tunes na lista de projetos

--- src/Data/projects.js
+++ src/Data/projects.js
@@ -1,8 +1,16 @@
 import pixelsArt from "../assets/projects/pixels-art.png";
+import trybeTunes from "../assets/projects/login.jpg";
 
 const projects = [
   {
     id: 1,
+    name: "Trybe Tunes",
+    image: trybeTunes,
+    link: "https://trybetunes-phi.vercel.app/",
+    github: "https://github.com/Fernanda9421/trybe-tunes",
+  },
+  {
+    id: 2,
     name: "Pixels Art",
     image: pixelsArt,
     link: "https://pixels-art-three.vercel.app/",
